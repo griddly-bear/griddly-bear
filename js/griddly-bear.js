d8c7d9bfeacc9dcdf9b8b43d59e8344a5b552ff1
Brandon Freeman <freemabr@gmail.com>
2013-08-21 11:58:05 -0400

    grid now has a reloading function

--- js/griddly-bear.js
+++ js/griddly-bear.js
@@ -604,7 +604,9 @@ $.widget('gb.grrr', {
         }
     },
     reloadGrid: function() {
-
+        $('.gb-grid table tbody').html('');
+        $('.gb-grid table thead th').removeClass('gb-hidden');
+        this._getRows();
     },
     toggleFilters: function()
     {
